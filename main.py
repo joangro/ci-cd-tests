0c7315e15ec6e9c0e11d47a1f823101498b188ee
Joan Grau <jgraunoel@gmail.com>
2020-02-25 13:15:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/')
+app.route("/")
 def index():
     return "Hello world!"
     
+app.route("/test")
+def testing():
+    return "Hello world!"
+    
+
 if __name__=='__main__':
     app.run('0.0.0.0', port=8080, debug=True)
