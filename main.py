8d43fd10c97e2fb0589ff4ab248d56e2abbadf89
Joan Grau <jgraunoel@gmail.com>
2020-02-25 12:10:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from Flask import flask
+
+app = Flask(__name__)
+
+app.route('/')
+def index():
+    return "Hello world!"
+    
+if __name__=='__main__':
+    app.run('127.0.0.1', port=8080, debug=True)
