dde89f759744ef61ef2dfede3b86c2695db9cf80
Joan Grau <jgraunoel@gmail.com>
2020-02-25 13:26:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def index():
     return "Hello world!"
     
-app.route("/test")
+@app.route("/test")
 def testing():
     return "Hello world!"
     
